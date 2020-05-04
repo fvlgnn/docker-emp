14d1ac083cfd4e7d461adf34bb055933eccff6f2
fvlgnn <fvlgnn@gmail.com>
2020-05-04 23:36:48 +0200

    optimize db volume

--- docker/mariadb/init/dbapp.sql
+++ docker/mariadb/init/dbapp.sql
@@ -12,11 +12,11 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
 
--- Dump database dbtest
-CREATE DATABASE IF NOT EXISTS `dbtest` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `dbtest`;
+-- Dump database dbapp
+CREATE DATABASE IF NOT EXISTS `dbapp` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `dbapp`;
 
--- Dump table dbtest.tblone
+-- Dump table dbapp.tblone
 CREATE TABLE IF NOT EXISTS `tblone` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(64) DEFAULT NULL,
@@ -25,7 +25,7 @@ CREATE TABLE IF NOT EXISTS `tblone` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
--- Dump table dbtest.tbltwo
+-- Dump table dbapp.tbltwo
 CREATE TABLE IF NOT EXISTS `tbltwo` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `oneId` int(11) NOT NULL,
@@ -35,7 +35,7 @@ CREATE TABLE IF NOT EXISTS `tbltwo` (
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
--- Dump della struttura di tabella dbtest.users
+-- Dump della struttura di tabella dbapp.users
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `permit` int(1) NOT NULL COMMENT,
@@ -48,9 +48,9 @@ CREATE TABLE IF NOT EXISTS `users` (
   UNIQUE KEY `email` (`email`)
 ) ENGINE=MyISAM AUTO_INCREMENT=45 DEFAULT CHARSET=utf8;
 
--- Dump dei dati della tabella dbtest.users: 1 rows
+-- Dump dei dati della tabella dbapp.users: 1 rows
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` (`id`, `permit`, `email`, `password`, `name`, `surname`) SELECT 1, 9, 'admin@domain.ltd', '5f4dcc3b5aa765d61d8327deb882cf99', 'Name', 'Surname' FROM DUAL WHERE NOT EXISTS (SELECT * FROM `users`) LIMIT 1;
+--INSERT INTO `users` (`id`, `permit`, `email`, `password`, `name`, `surname`) SELECT 1, 9, 'admin@domain.ltd', '5f4dcc3b5aa765d61d8327deb882cf99', 'Name', 'Surname' FROM DUAL WHERE NOT EXISTS (SELECT * FROM `users`) LIMIT 1;
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
